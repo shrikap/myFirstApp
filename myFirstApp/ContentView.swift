765b3332a01730d27facefea33aee7592e4c5227
shrikap <shrikapunuganti@gmail.com>
2023-07-12 14:34:57 -0700

    Initial Commit

---
+++ myFirstApp/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  myFirstApp
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+        VStack {
+            Text("Kode with Klossy scholars!")
+            .font(.title)
+            .foregroundColor(Color.pink)
+            
+            Image("scholars")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+        }
+    
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
